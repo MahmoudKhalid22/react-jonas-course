e453e916889b3d963e310007088a9250bba9a3ce
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-01-06 21:03:30 +0200

    fragment

--- sec-5-components-props-jsx/pizza/src/index.tsx
+++ sec-5-components-props-jsx/pizza/src/index.tsx
@@ -72,12 +72,19 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {pizzas ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu, please come back later.</p>
       )}
