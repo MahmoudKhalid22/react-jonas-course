310501a2429293f9b426cb3b5f5cbd0802f96e71
MahmoudKhalid <mahmoud0122549@gmail.com>
2024-01-11 22:56:15 +0200

    creating components

--- sec-6-state-events-forms-interactive_components/travel-list/src/App.tsx
+++ sec-6-state-events-forms-interactive_components/travel-list/src/App.tsx
@@ -1,8 +1,69 @@
 import React from "react";
-import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+];
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😎 trip?</h3>{" "}
+      <select>
+        {Array.from({ length: 21 }, (_, index) => index++).map((index) => (
+          <option key={index} value={index}>
+            {index}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+function PackList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }: any) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App;
